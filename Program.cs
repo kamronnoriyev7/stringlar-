53db1a53fcec8f7d7f4726bb3d1f8071fd4783eb
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-13 12:15:40 +0500

    .

---
+++ Program.cs
@@ -0,0 +1,204 @@
+﻿// HOMEWORK 
+
+// public class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.Write("satr: ");
+//         var input = Console.ReadLine();
+//         string result;
+//         Function(input, out result);
+//         Console.WriteLine(result);
+//     }
+//
+//     public static void Function(string input, out string result)
+//     {
+//         result = string.Empty;
+//         for (int i = 0; i < input.Length; i++)
+//         {
+//             result += input[input.Length - 1 - i];
+//         }
+//     }
+// }
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    public static void Test1()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
